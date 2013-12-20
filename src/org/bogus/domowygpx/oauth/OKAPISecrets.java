2c0fa00685b1f0b8a2f86422a4aed69e514481cf
Bogusław Szczepanowski <f_ranek@o2.pl>
2013-12-20 01:18:53 +0100

    Moved class

---
+++ src/org/bogus/domowygpx/oauth/OKAPISecrets.java
@@ -0,0 +1,11 @@
+package org.bogus.domowygpx.oauth;
+
+import android.content.Context;
+
+class OKAPISecrets
+{
+    static String getApiKey(Context ctx)
+    {
+        return "Consumer Key|Consumer Secret";
+    }
+}
