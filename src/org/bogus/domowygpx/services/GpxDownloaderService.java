f67bc45bbdd7250c4bb156bd5cf0eb04224ef7a3
Bogusław Szczepanowski <f_ranek@o2.pl>
2014-01-11 22:45:59 +0100

    The option to include/suppress travelbugs in the cache description

--- src/org/bogus/domowygpx/services/GpxDownloaderService.java
+++ src/org/bogus/domowygpx/services/GpxDownloaderService.java
@@ -372,7 +372,9 @@ public class GpxDownloaderService extends Service implements GpxDownloaderApi
             retrParams.put("ns_ox", "true");
             retrParams.put("ns_gsak", "true");
             retrParams.put("images", "descrefs:all");
-            retrParams.put("trackables", "desc:list");
+            if (config.getBoolean("listTrackables", true)){
+                retrParams.put("trackables", "desc:list");
+            }
             retrParams.put("recommendations", "desc:count");
             retrParams.put("attrs", "gc:attrs|desc:text|gc_ocde:attrs"); // do I need ox:tags ?
             retrParams.put("alt_wpts", "true");
